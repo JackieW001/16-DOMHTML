ce21373a3428b94cb46b4f3b4812d27bead05741
JackieW001 <jwoo@stuy.edu>
2017-12-09 17:19:41 -0500

    change h1 when hovering

--- intro.js
+++ intro.js
@@ -7,8 +7,22 @@ var addToList = function(e){
     ctr += 1;
 };
 
-
 // click button to add element to list
 var button = document.getElementById('b');
 button.addEventListener('click', addToList );
 
+var setHead = function(text){
+    var head = document.getElementById('h');
+    console.log(text);
+    head.innerHTML = text;
+};
+
+document.body.addEventListener('mouseover', function(e) { console.log('hi');
+    if (e.target.nodeName == 'LI'){
+	console.log(e.target.innerHTML);
+	setHead(e.target.innerHTML);
+    }
+    else{
+	setHead("Hello World!");
+    }    
+ });
